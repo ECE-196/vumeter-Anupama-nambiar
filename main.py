9fb6577f1615b67e7cc9e0bf437b04a31c6332d1
Anupama-nambiar <annambiar@ucsd.edu>
2024-10-16 23:03:54 -0700

    edited_main

--- main.py
+++ main.py
@@ -10,9 +10,19 @@ status = DigitalInOut(board.IO17)
 status.direction = Direction.OUTPUT
 
 led_pins = [
-    board.IO21,
-    board.IO26, # type: ignore
-    board.IO47,
+
+    board.IO21, 
+    board.IO26, # type: ignore  
+    board.IO47, #not w
+    board.IO33, # type: ignore
+    board.IO34, # type: ignore
+    board.IO48, #not w
+    board.IO35, 
+    board.IO36, 
+    board.IO37, 
+    board.IO38, 
+    board.IO39  
+
     # do the rest...
 ]
 
@@ -21,17 +31,37 @@ leds = [DigitalInOut(pin) for pin in led_pins]
 for led in leds:
     led.direction = Direction.OUTPUT
 
+volume_prev=0 #initializing the previous volume to 0
+i=0           #initializing i to 0
+
 # main loop
 while True:
+    
+    
     volume = microphone.value
+    
+    diff=volume-volume_prev #calculates the difference between current volume and previously read volume 
+    print(diff)
 
-    print(volume)
+    
+    
+    if abs(diff)>1400:       #when the difference in the volume is greater than a certain threshold (I have set it to 1400 based on the readings in my environment)
+        leds[i].value = True #It depicts the fact that there has been an increase in volume and hence turns on more leds as the change is detected
+        i+=1            
+        if i>10:              #Ensures that we don't go out of bounds when indexing the list "leds" (since we have 0-10 indices)
+            i=10
+        
+    
+    else:                      #when the volume is decreased the change is usually gradually read and hence it is not a drastic fall as big as my threshold value
+        leds[i].value = False  #hence it decreases the number of leds turned on one at a time
+        i-=1
+        if i<0:
+            i=0                 #this line ensures we don't index negatively
+    
+    
+    volume_prev=microphone.value
+    
+    sleep(0.2)
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
-
-    sleep(1)
-
-    # instead of blinking,
-    # how can you make the LEDs
-    # turn on like a volume meter?
+    
+ 
